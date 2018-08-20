250f76efa3ccbc4d6024d71934cb6a7f365a6793
Brady Vercher <brady@blazersix.com>
2018-08-20 11:18:26 -0700

    Fix the new test. It was nested in a different test.

--- test/makepot.js
+++ test/makepot.js
@@ -48,27 +48,27 @@ test('makepot no changes', function(t) {
     var pot = gettext.po.parse(fs.readFileSync(potFilename, 'utf8'));
     t.equal(pot.headers['pot-creation-date'], creationDate, 'the creation date should not change');
   });
+});
 
-  /**
-   * @link https://github.com/cedaro/node-wp-i18n/issues/16
-   */
-  test('makepot when working directory is not package root', function(t) {
-    t.plan(3);
+/**
+ * @link https://github.com/cedaro/node-wp-i18n/issues/16
+ */
+test('makepot when working directory is not package root', function(t) {
+  t.plan(3);
 
-    makepot({
-      cwd: path.resolve('tmp/makepot/nested-theme'),
-      domainPath: '/languages',
-      mainFile: 'subdir/style.css',
-      potFile: 'nested-theme.pot',
-      type: 'wp-theme'
-    }).then(function() {
-      var potFilename = path.resolve('tmp/makepot/nested-theme/languages/nested-theme.pot');
-      t.ok(fs.statSync(potFilename));
+  makepot({
+    cwd: path.resolve('tmp/makepot/nested-theme'),
+    domainPath: '/languages',
+    mainFile: 'subdir/style.css',
+    potFile: 'nested-theme.pot',
+    type: 'wp-theme'
+  }).then(function() {
+    var potFilename = path.resolve('tmp/makepot/nested-theme/languages/nested-theme.pot');
+    t.ok(fs.statSync(potFilename));
 
-      var pot = gettext.po.parse(fs.readFileSync(potFilename, 'utf8'));
-      var themeName = 'Example Theme';
-      t.equal(pot.headers['project-id-version'], themeName, 'the theme name should be the project id in the pot file');
-      t.equal(pot.translations[''][ themeName ]['msgid'], themeName, 'the theme name should be included as a string in the pot file');
-    });
+    var pot = gettext.po.parse(fs.readFileSync(potFilename, 'utf8'));
+    var themeName = 'Example Theme';
+    t.equal(pot.headers['project-id-version'], themeName, 'the theme name should be the project id in the pot file');
+    t.equal(pot.translations[''][ themeName ]['msgid'], themeName, 'the theme name should be included as a string in the pot file');
   });
 });
